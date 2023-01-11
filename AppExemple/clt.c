19a6cad2bcfba157d9ca924c38749c38b5d893ec
Pierre GALOPIN <78901002+pierreGLP@users.noreply.github.com>
2023-01-11 18:54:59 +0100

    push

--- AppExemple/clt.c
+++ AppExemple/clt.c
@@ -5,7 +5,10 @@ int main(int argc, char const *argv[])
 {
     int sock = creerSocket(SOCK_STREAM);
     connectSrv(sock, "127.0.0.1", 5000);
-    identificationClient(sock);
+    dialogueClt(sock);
+
+
     close(sock);
     return 0;
 }
+
--- session/cltDgram.c
+++
@@ -1,5 +0,0 @@
-int main(int argc, char const *argv[])
-{
-    s=creerSocket(DGRAM)
-    return 0;
-}
